4362270875b3b6f3a803fc6ca8ab917d3a2575a6
R. Dunbar Poor <rdpoor@gmail.com>
2020-06-27 19:12:19 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="mcp2210",
-      version="0.1.5",
+      version="0.1.6",
       description="Python interface for the MCP2210 USB-SPI interface",
       author="Robert Poor",
       author_email="rdpoor@gmail.com",
